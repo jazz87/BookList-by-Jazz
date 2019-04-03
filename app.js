38a39daf952f9f962fa004895909832a07ccbb92
Jasbee <info@jasbeesingh.nl>
2019-04-03 16:03:03 +0200

    Finished!
    Added Function that handles storage.

--- app.js
+++ app.js
@@ -10,20 +10,7 @@ class Book {
 // UI Class: Handle UI Tasks
 class UI {
     static displayBooks() {
-        const StoredBooks = [
-            {
-                title: 'Book One',
-                author: 'John Doe',
-                isbn: '7894561'
-            },
-            {
-                title: 'Book Two',
-                author: 'Jane Doe',
-                isbn: '1234567'
-            }
-        ];
-
-        const books = StoredBooks;
+        const books = Store.getBooks();
 
         books.forEach((book) => UI.addBookToList(book));
     }
@@ -69,6 +56,36 @@ class UI {
 }
 
 // Store Class: Handles Storage
+class Store {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach((book, index) => {
+            if(book.isbn === isbn){
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
@@ -92,9 +109,12 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
         const book = new Book(title, author, isbn);
 
         // Add Book to UI
-        UI.addBookToList(book); 
+        UI.addBookToList(book);
+        
+        // Add book to storage
+        Store.addBook(book);
 
-        // Show succes message
+        // Show succes message when book is added
         UI.showAlert('Boek Toegevoegd', 'success');
 
         // Clear fields
@@ -104,5 +124,12 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
-    UI.deleteBook(e.target)
+    // Remove Book from UI
+    UI.deleteBook(e.target);
+
+    // Remove Book from Storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+    // Show succes message when book is removed
+    UI.showAlert('Boek Verwijderd', 'success');
 });
